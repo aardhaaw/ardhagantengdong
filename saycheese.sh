bc6a405117cce70cfbd9f6d572b9c29d58c40cbd
aardhaaw <68586071+aardhaaw@users.noreply.github.com>
2020-07-21 16:02:16 +0700

    Update saycheese.sh

--- saycheese.sh
+++ saycheese.sh
@@ -8,7 +8,7 @@ trap 'printf "\n";stop' 2
 banner() {
 
 
-printf "\e[1;92m  ____              \e[0m\e[1;77m ____ _                          \e[0m\n"
+printf "\e[1;92m  ____ ardhaganteng \e[0m\e[1;77m ____ _  ardhagantengbanget     \e[0m\n"
 printf "\e[1;92m / ___|  __ _ _   _ \e[0m\e[1;77m/ ___| |__   ___  ___  ___  ___  \e[0m\n"
 printf "\e[1;92m \___ \ / _\` | | | \e[0m\e[1;77m| |   | '_ \ / _ \/ _ \/ __|/ _ \ \e[0m\n"
 printf "\e[1;92m  ___) | (_| | |_| |\e[0m\e[1;77m |___| | | |  __/  __/\__ \  __/ \e[0m\n"
